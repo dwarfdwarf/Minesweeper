9c4ef30a52206eab71354fea3b6f576ecb07e8e2
dwarfdwarf <47924391+dwarfdwarf@users.noreply.github.com>
2024-12-23 09:57:22 -0600

    completed reveal mechanic

--- main.py
+++ main.py
@@ -17,24 +17,22 @@ visibleBoard = np.zeros((height, width), dtype='str')
 def checkIfBomb(x, y):
     return (board[x,y]==-1)
 
+def checkIfRevealed(x, y):
+    return (visibleBoard[x,y]!="X")
+
 def revealSpot(x, y):
-    if not checkIfBomb(x,y):
+    if not checkIfBomb(x,y) and not checkIfRevealed(x,y):
+        visibleBoard[x,y]=board[x,y]
         if board[x,y]==0:
             revealSurrounding(x,y)
-        visibleBoard[x,y]=board[x,y]
         return True
     return False
 
 def revealSurrounding(x, y):
-    for x in range(height):
-        for y in range(width):
-            if board[x,y] != -1:
-                counter = 0
-                for i in range(-1, 2):
-                    for j in range(-1, 2):
-                        if x+i>=0 and x+i<height and y+j>=0 and y+j<width and board[x+i,y+j] == -1:
-                            counter += 1
-                board[x,y] = counter
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if x+i>=0 and x+i<height and y+j>=0 and y+j<width:
+                revealSpot(x+i,y+j)
     return
 
 def printBoard():
@@ -83,3 +81,13 @@ for x in range(height):
         visibleBoard[x,y] = "X"
 
 printBoard()
+
+while True:
+    inX = int(input("enter x:"))
+    inY = int(input("enter y:"))
+    if checkIfBomb(inX, inY):
+        printBoard()
+        print("YOU LOSE!")
+        break
+    revealSpot(inX, inY)
+    printBoard()
